0c3ba66acf172c602de3c40f9da106ff641a6c27
Flavio H Freitas <flaviohenriquecbc@gmail.com>
2018-06-09 14:52:25 +0200

    Accepts nested lists - fix

--- htmlToElement.js
+++ htmlToElement.js
@@ -1,135 +1,198 @@
-import { StyleSheet, Platform } from 'react-native';
-import _ from 'underscore';
+import React from 'react';
+import {StyleSheet, Text, Dimensions, View} from 'react-native';
+import htmlparser from 'htmlparser2-without-node-native';
+import entities from 'entities';
 
-import colors from './color.enum.style';
-import { fontFamily } from './font.enum.style';
-// import textStyles from './../../style/text.style.js';
+import AutoSizedImage from './AutoSizedImage';
 
-function create(params) {
-    const defaultText = _.extend({
-        fontFamily: fontFamily.regular,
-        fontSize: 16,
-        lineHeight: Platform.OS === 'android' ? 32 : 21,
-        color: colors.black
-    }, params || {});
-    const defaultH = {fontFamily: fontFamily.bold, color: colors.black};
-    const boldStyle = {...defaultText, fontFamily: fontFamily.bold};
-    const italicStyle = {...defaultText, fontStyle: 'italic', fontFamily: fontFamily.italic};
-    const underlineStyle = {...defaultText, textDecorationLine: 'underline'};
-    const codeStyle = {...defaultText, fontFamily: Platform.OS === 'ios' ? 'Menlo' : 'monospace'};
+const defaultOpts = {
+  lineBreak: '\n',
+  paragraphBreak: '\n',
+  bullet: '\u2022 ',
+  TextComponent: Text,
+  textComponentProps: null,
+  NodeComponent: Text,
+  nodeComponentProps: null,
+};
 
-    return StyleSheet.create({
-        b: boldStyle,
-        strong: boldStyle,
-        i: italicStyle,
-        em: italicStyle,
-        u: underlineStyle,
-        pre: codeStyle,
-        code: codeStyle,
-        a: {
-            ...defaultText,
-            color: colors.green,
-            textDecorationLine: 'underline',
-        },
-        p: {
-            ...defaultText
-        },
-        div: {
-            ...defaultText
-        },
-        strike: {
-            ...defaultText,
-            textDecorationLine: 'line-through'
-        },
-        blockquote: {
-            // ...italicStyle,
-            padding: 0,
-            borderLeftWidth: 5,
-            borderLeftColor: '#4dbfbf',
-            paddingHorizontal: 15,
-            paddingVertical: 10,
-            marginBottom: 30,
-        },
-        cite: {
-            ...defaultText,
-            fontFamily: fontFamily.bold,
-            fontSize: 12
-        },
-        abbr: {
-            ...defaultText,
-            fontSize: 14,
-            color: colors.darkGray,
-            textDecorationStyle: 'dotted',
-            textDecorationLine: 'underline'
-        },
-        h1: {...defaultH, fontSize: 20, lineHeight: Platform.OS === 'android' ? 34 : 25},
-        h2: {...defaultH, fontSize: 18, lineHeight: Platform.OS === 'android' ? 32 : 23},
-        h3: {...defaultH, fontSize: 16, lineHeight: Platform.OS === 'android' ? 30 : 21},
-        h4: {...defaultH, fontSize: 14, lineHeight: Platform.OS === 'android' ? 28 : 19},
-        h5: {...defaultH, fontSize: 12, lineHeight: Platform.OS === 'android' ? 26 : 17},
-        h6: {...defaultH, fontSize: 10, lineHeight: Platform.OS === 'android' ? 24 : 15},
-        ol: {
-            // color: colors.pink,
-            paddingHorizontal: 10,
-            paddingTop: 10
-        },
-        ul: {
-            // color: colors.pink,
-            paddingHorizontal: 10,
-            paddingTop: 10
-        },
-        li: {
-            // ...defaultText,
-            flexDirection: 'row',
-            marginBottom: 10,
-        },
-        itemText: {
-            ...defaultText,
-            flex: 1,
-            paddingLeft: 5
-        },
-        listBullet: {
-            color: colors.pink,
-            fontSize: 15,
-            marginTop: 1,
-            lineHeight: 15,
-            width: 10
-        },
-        listNumber: {
-            ...boldStyle,
-            color: colors.pink,
-            width: 25,
-        },
-        hr: {
-            backgroundColor: colors.grayDivider,
-            height: 1,
-            marginVertical: 10,
-            marginBottom: 40,
-        },
-        figure: {
-            flex: 1,
-            alignContent: 'center',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            marginBottom: 10
-        },
-        figcaption: {
-            ...defaultText,
-            fontSize: 10,
-            color: colors.darkGray,
-            textAlign: 'center',
-            width: '100%'
-        },
-        img: {
-            marginBottom: 10,
+const Img = props => {
+  const { width } = Dimensions.get('window');
+  const imgStyle = {
+    maxWidth: width,
+    resizeMode: 'cover',
+  };
+
+  const source = {
+    uri: props.attribs.src,
+    width,
+  };
+
+  // if no src is passed, add generic image
+  if (!props.attribs.src) {
+    imgStyle.width = 16;
+    imgStyle.height = 16;
+    source.uri = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAABHNCSVQICAgIfAhkiAAAAWRJREFUOI21k7FLAnEUx9/vFF08rzjw8oImTZoi6A9odwpcIhyCxubW3Kr1FCIcRM4llRKCBtdybLbi5+Lw87w47DjJBvU1xC/0upIb+m7v8b6f33vvxyOINKvrjVPbdhTwIUmKmNns7jEplc4NxxnG4nHlxQ+g1zOTkiT2g44zjKmq8pzJHG34AdTrhTZj/ZRAiDBBBOLHDACACIQQYSL4Nbr1fwBE6vkriFRCpN8jewIQ6V61eveASPdd+XSr9XgNAEAIoCcAke4Ui1cXhvGa0LRyBZHmEKmKSHP5fPm23e5sEZJAXh90mTd1vVEZjT4kntO08oksLx9Y1mANACAcDr3/2AFvp9m8vxwM7FV3V9zspbkRul2W+q2Qazweh2bjuRFUVelY1tvKXwBZXjLmAIjTAA/S6cPtRR3MCnEaCIpixGTMXK/VCk98F4uNQBgzk9Go2Cdf53xzZtvDmJ/X+Tl/ArQ4nue4vQorAAAAAElFTkSuQmCC';
+  }
+
+  return <AutoSizedImage source={source} style={imgStyle} />;
+};
+
+export default function htmlToElement(rawHtml, customOpts = {}, done) {
+  const opts = {
+    ...defaultOpts,
+    ...customOpts,
+  };
+
+  function inheritedStyle(parent) {
+    if (!parent) return null;
+    const style = StyleSheet.flatten(opts.styles[parent.name]) || {};
+    const parentStyle = inheritedStyle(parent.parent) || {};
+    return {...parentStyle, ...style};
+  }
+
+  function domToElement(dom, parent) {
+    if (!dom) return null;
+
+    const renderNode = opts.customRenderer;
+    let orderedListCounter = 1;
+
+    return dom.map((node, index, list) => {
+      if (renderNode) {
+        const rendered = renderNode(
+          node,
+          index,
+          list,
+          parent,
+          domToElement
+        );
+        if (rendered || rendered === null) return rendered;
+      }
+
+      const {TextComponent} = opts;
+
+      if (node.type === 'text') {
+        const defaultStyle = opts.textComponentProps ? opts.textComponentProps.style : null;
+        const customStyle = inheritedStyle(parent);
+        const text = parent && parent.name === 'figcaption' ? (node.data && node.data.toUpperCase()) : node.data;
+        const liStyle = parent && parent.name === 'li' ? inheritedStyle({name: 'itemText'}) : {};
+        return (
+          <TextComponent
+            {...opts.textComponentProps}
+            key={index}
+            style={[defaultStyle, customStyle, liStyle]}
+          >
+            {entities.decodeHTML(text)}
+          </TextComponent>
+        );
+      }
+
+      if (node.type === 'tag') {
+        if (node.name === 'img') {
+          return <Img key={index} attribs={node.attribs} />;
         }
+
+        let linkPressHandler = null;
+        let linkLongPressHandler = null;
+        if (node.name === 'a' && node.attribs && node.attribs.href) {
+          let link = entities.decodeHTML(node.attribs.href);
+          // remove not expected codes
+          link = link && link.replace(/^'|"|”/, '').replace(/$'|"|”/, '');
+          linkPressHandler = () =>
+            opts.linkHandler(link);
+          if (opts.linkLongPressHandler) {
+            linkLongPressHandler = () =>
+              opts.linkLongPressHandler(link);
+          }
+        }
+
+        let linebreakBefore = null;
+        let linebreakAfter = null;
+        if (opts.addLineBreaks) {
+          switch (node.name) {
+          case 'pre':
+          case 'footer':
+            linebreakBefore = opts.lineBreak;
+            break;
+          case 'p':
+            if (index < list.length - 1) {
+              linebreakAfter = opts.paragraphBreak;
+            }
+            break;
+          case 'br':
+          case 'h1':
+          case 'h2':
+          case 'h3':
+          case 'h4':
+          case 'h5':
+          case 'h6':
+          case 'div':
+          case 'figure':
+            linebreakAfter = opts.lineBreak;
+            break;
+          }
+        }
+
+        const {NodeComponent, styles} = opts;
+
+        const children = [linebreakBefore, domToElement(node.children, node), linebreakAfter];
+        let hasView = false;
+
+        if (node.name === 'ul' || node.name === 'ol' || node.name === 'figure' || node.name === 'blockquote') {
+          hasView = true;
+          children = domToElement(node.children, node);
+        } else if (node.name === 'li') {
+          hasView = true; 
+          children = [
+            <TextComponent key={`${index}-1`} style={inheritedStyle({name: parent.name === 'ol' ? 'listNumber' : 'listBullet'})}>
+              {parent.name === 'ol' ? `${orderedListCounter++}.` : opts.bullet}
+            </TextComponent>,
+            <TextComponent key={`${index}-2`} style={inheritedStyle({name: 'itemText'})}>
+              {domToElement([node.children[0]], node)}
+            </TextComponent>
+          ];
+          if (node.children.length > 1) {
+            return (
+              [
+                renderChildrenView(node, styles, index, children),
+                domToElement([node.children[1]], node)
+              ]
+            )
+          }
+        }
+
+        if (hasView) {
+          return renderChildrenView(node, styles, index, children);
+        }
+
+        return <NodeComponent
+            {...opts.nodeComponentProps}
+            key={index}
+            onPress={linkPressHandler}
+            style={[
+              !node.parent ? styles[node.name] : null,
+            ]}
+            onLongPress={linkLongPressHandler}
+          >
+            {children}
+          </NodeComponent>;
+      }
     });
+  }
+
+  function renderChildrenView(node, styles, index, children) {
+      return <View
+      style={!node.parent || node.name === 'li' ? styles[node.name] : null}
+      key={index}
+    >
+      {children}
+    </View>;
+  }
+
+  const handler = new htmlparser.DomHandler(function(err, dom) {
+    if (err) done(err);
+    done(null, domToElement(dom));
+  });
+  const parser = new htmlparser.Parser(handler);
+  parser.write(rawHtml);
+  parser.done();
 }
-
-
-export function createStyle(params) {
-    return create(params);
-}
-
-export default create();
